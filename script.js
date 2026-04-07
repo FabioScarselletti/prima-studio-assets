2520cd183492ef2ebad38db955b78f3544c79d00
Fabio Scarselletti <74779509+FabioScarselletti@users.noreply.github.com>
2026-04-07 12:47:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission alert
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! We'll get back to you shortly.");
+});
